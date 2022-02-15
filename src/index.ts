098ceacdcc6c6c71a799008c010cbc8e33afb930
rscoopcur <rscoop@cursoft.cw>
2022-02-15 09:56:59 -0400

    Update to include enums

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { nanoid } from 'nanoid';
-import { ArrowKeys, arrowTypes, ErdShape } from './erdShape';
 import * as fs from 'fs/promises';
 import inquirer from 'inquirer';
 import * as process from 'process';
 import { FuzzyPathQuestionOptions } from 'inquirer-fuzzy-path';
-import { Database, Endpoint, RelationalId } from './types/types';
-import { buildColumnName, getFieldType } from './helpers';
+import { Database } from './types/types';
 
 const { Parser, ModelExporter } = require('@dbml/core');
 
@@ -19,7 +16,10 @@ function titleCase(aString: string): string {
 
 function GetTableName(aTableName: string): string {
   // TODO if there are spaces in the 
-  aTableName = aTableName;
+  // TODO fix the function Currently it changes UserTypes to Usertypes 
+  if (aTableName.indexOf(' ') >= 0) {
+    aTableName = titleCase(aTableName);
+  }
   const searchRegExp = /\s/g;
   return aTableName.replace(searchRegExp, '');
 }
@@ -51,39 +51,79 @@ async function main() {
   ]);
 
   const inputFile = inputFileResponse.path;
-
-  const dmbl = await fs.readFile(inputFile, 'utf-8');
-
-  const parsedDbml = Parser.parse(dmbl, 'dbml');
-  const dmblJson = ModelExporter.export(parsedDbml, 'json', false);
-  const database = JSON.parse(dmblJson) as Database;
+  const dbml = await fs.readFile(inputFile, 'utf-8');
+  const parsedDbml = Parser.parse(dbml, 'dbml');
+  const dbmlJson = ModelExporter.export(parsedDbml, 'json', false);
+  const schemas = JSON.parse(dbmlJson);
+  //console.log(schemas.schemas[0].enums[0].values[0]);
+  const database = schemas as Database;
   let schemaPUML = [] as string[];
   schemaPUML.push("@startuml")
   schemaPUML.push("")
   schemaPUML.push("!theme plain")
   schemaPUML.push("hide circle")
   schemaPUML.push("skinparam linetype ortho")
+  schemaPUML.push("title Add Title Here")
   schemaPUML.push("")
 
+  schemas.schemas.forEach((schema:any) => {
+    schema.enums.forEach((anEnum:any) => {
+      schemaPUML.push("enum " + anEnum.name + " {");
+      anEnum.values.forEach((aValue:any)=>{
+          schemaPUML.push("  " + aValue.name );
+      })
+      schemaPUML.push("}")
+      schemaPUML.push("")
+    });
+  });
+  let schemaIndex = -1;
   database.schemas.forEach(schema => {
 
-    schema.tables.forEach(table => {
-      schemaPUML.push("entity " + GetTableName(table.name) + " {")
+    schemaIndex++;
 
+    schema.tables.forEach(table => {
+
+      let enums = [] as string[];
+
+      schemaPUML.push("entity " + GetTableName(table.name) + " {")
+      let hasPK = false;
       table.fields.forEach(field => {
-        let tempFieldname = " " + field.name
+        let tempFieldname = " " + field.name + " : " + field.type.type_name
         if (field.pk) {
+          hasPK = true
           tempFieldname = " *" + tempFieldname
+          if (field.increment) {
+            tempFieldname = tempFieldname + " <<autoinc>> "
+          }
+          schemaPUML.push(tempFieldname);
         }
-        if (field.increment) {
-          tempFieldname = tempFieldname + " <<autoinc>> "
+      });
+      if (hasPK) {
+        schemaPUML.push(" --")
+      }
+      table.fields.forEach(field => {
+        let tempFieldname = " " + field.name + " : " + field.type.type_name
+        if (!field.pk) {
+          if (field.increment) {
+            tempFieldname = tempFieldname + " <<autoinc>> "
+          }
+          let anIndex = schemas.schemas[schemaIndex].enums.findIndex((x:any)=>{return x.name == field.type.type_name});
+          if (anIndex > -1) {
+            enums.push(GetTableName(table.name) +  " -- " + field.type.type_name)
+          }
+          schemaPUML.push(tempFieldname);
         }
-        schemaPUML.push(tempFieldname);
       });
 
       schemaPUML.push("}")
       schemaPUML.push("")
+      if (enums.length > 0) {
 
+        enums.forEach((anEnum:any)=>{
+          schemaPUML.push(anEnum)
+        })
+        schemaPUML.push("")
+      }
 
     });
 
@@ -98,7 +138,7 @@ async function main() {
       schemaPUML.push(StartPoint + "--" + EndPoint)
 
     });
-    
+
   });
   schemaPUML.push("")
 
@@ -109,7 +149,9 @@ async function main() {
     return;
   }
   (async () => {
-  await fs.writeFile(`dist/files/${filenameMatches[1]}.puml`, schemaPUML.join("\n"));
+  
+    //await fs.writeFile(`dist/files/${filenameMatches[1]}.json`, dmblJson);
+    await fs.writeFile(`dist/files/${filenameMatches[1]}.puml`, schemaPUML.join("\n"));
   })();
 
 }
